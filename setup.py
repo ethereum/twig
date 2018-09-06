50c997dcfe33efe1c3a562f3baa6cc98ecffb8f6
Nick Gheorghita <nickgheorghita@gmail.com>
2018-09-06 16:54:07 -0600

    Bump version: 0.1.0-alpha.2 → 0.1.0-alpha.3

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ extras_require['dev'] = (
 setup(
     name='twig',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.2',
+    version='0.1.0-alpha.3',
     description="""twig: A tool for Ethereum smart contract development.""",
     long_description_markdown_filename='README.md',
     author='Jason Carver',
